0afed6046f302f921a534d91f0e537829fc74989
Victor Moscone <victormoscone@gmail.com>
2020-11-04 21:26:50 -0800

    Style: Improved styling of the city details section.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let callCity = function asd(cityName) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=Fresno&appid=bb4f4eb722b35b0afd1d0fc61d673140')
-        .then(response => response.json())
-        .then(data => console.log(data));
-}
+// let callCity = function asd(cityName) {
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q=Fresno&appid=bb4f4eb722b35b0afd1d0fc61d673140')
+//         .then(response => response.json())
+//         .then(data => console.log(data));
+// }
 
-callCity();
+// callCity();
