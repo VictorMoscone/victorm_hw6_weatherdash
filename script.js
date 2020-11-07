86b4d6966b6abd9eb3f71c980509c9ac5611e60f
Victor Moscone <victormoscone@gmail.com>
2020-11-06 19:59:13 -0800

    Feat: Setup the IDs and their respective fetches for the weather. Had to include the Moment library.

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
+let cityName = document.querySelector("#cityName");
+let currentDate = document.querySelector("#currentDate");
+let tempF = document.querySelector("#tempF");
+let humidity = document.querySelector("#humidity");
 let windSpeed = document.querySelector("#windSpeed");
-let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Fresno&appid=bb4f4eb722b35b0afd1d0fc61d673140';
+let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Fresno&appid=bb4f4eb722b35b0afd1d0fc61d673140&units=imperial';
 
 function getApi(requestUrl) {
     fetch(requestUrl)
@@ -7,8 +11,15 @@ function getApi(requestUrl) {
         return response.json();
       })
       .then(function (data) {
-        windSpeed.textContent = data.wind.speed
+        console.log(data);
+        cityName.textContent = data.name;
+        currentDate.textContent = `(${moment().format('MM/D/YYYY')})`
+        tempF.textContent = data.main.temp;
+        humidity.textContent = data.main.humidity;
+        windSpeed.textContent = data.wind.speed;
       });
   }
   
   getApi(requestUrl);
+
+//   TODO: response.status to provide errors if the site was down or 404'd, etc.
