15e4dcbc26763ee2cbc676fcb1870cae03ce39ab
Victor Moscone <victormoscone@gmail.com>
2020-11-06 22:01:03 -0800

    Feat: Weather details now populates based on user input, via event listener.

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ let currentDate = document.querySelector("#currentDate");
 let tempF = document.querySelector("#tempF");
 let humidity = document.querySelector("#humidity");
 let windSpeed = document.querySelector("#windSpeed");
-let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Clovis&appid=bb4f4eb722b35b0afd1d0fc61d673140&units=imperial';
+let cityForm = document.querySelector("#cityForm");
+let chosenCity = document.querySelector("#chosenCity");
 
 function getApi(requestUrl) {
     fetch(requestUrl)
@@ -19,7 +20,10 @@ function getApi(requestUrl) {
         windSpeed.textContent = data.wind.speed;
       });
   }
-  
-  getApi(requestUrl);
+
+cityForm.addEventListener("submit", function(e) {
+    e.preventDefault();
+    getApi(`https://api.openweathermap.org/data/2.5/weather?q=${chosenCity.value}&appid=bb4f4eb722b35b0afd1d0fc61d673140&units=imperial`);
+})
 
 //   TODO: response.status to provide errors if the site was down or 404'd, etc.
