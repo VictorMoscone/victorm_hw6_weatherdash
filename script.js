d60de4127e064a03915ddea66f10a6c52bd3b8e2
Victor Moscone <victormoscone@gmail.com>
2020-11-06 20:10:11 -0800

    Feat: Adding the search form.

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let currentDate = document.querySelector("#currentDate");
 let tempF = document.querySelector("#tempF");
 let humidity = document.querySelector("#humidity");
 let windSpeed = document.querySelector("#windSpeed");
-let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Fresno&appid=bb4f4eb722b35b0afd1d0fc61d673140&units=imperial';
+let requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q=Clovis&appid=bb4f4eb722b35b0afd1d0fc61d673140&units=imperial';
 
 function getApi(requestUrl) {
     fetch(requestUrl)
